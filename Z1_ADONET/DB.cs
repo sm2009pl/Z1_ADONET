4d1a9a3005ed29ebacbe42a9143050cb3b8760a3
Szymon M <szimon.doto@hotmail.com>
2020-03-02 23:25:00 +0100

    Dodaj pliki projektów.

---
+++ Z1_ADONET/DB.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Data.SqlClient;
+namespace ADONET_1
+{
+	public class DB
+	{
+		public DB()
+		{
+		}
+
+		public void Select(SqlConnection connection)
+		{
+			var Sql = "SELECT * FROM Customers";
+			var command = new SqlCommand(Sql, connection);
+			SqlDataReader reader = command.ExecuteReader();
+			while (reader.Read())
+			{
+				Console.WriteLine(reader["CompanyName"]);
+			}
+			reader.Close();
+		}
+
+		public void Insert(SqlConnection connection, int id, string regionName)
+		{
+			var Sql = "INSERT INTO dbo.Region (RegionId, RegionDescription) VALUES (@id, @regionName)";
+			var command = new SqlCommand(Sql, connection);
+			command.Parameters.AddWithValue("@id", id);
+			command.Parameters.AddWithValue("@regionName", regionName);
+			int affected = command.ExecuteNonQuery();
+			Console.WriteLine($"{affected} rows affected");
+		}
+
+		public void Delete(SqlConnection connection, int id)
+		{
+			var Sql = "DELETE FROM dbo.Region WHERE (RegionID = @id)";
+			var command = new SqlCommand(Sql, connection);
+			command.Parameters.AddWithValue("@id", id);
+			command.ExecuteNonQuery();
+		}
+
+		public void Update(SqlConnection connection, int id, string regionName)
+		{
+			var Sql = "UPDATE Region SET RegionDescription = @regionName WHERE (RegionId = @id)";
+			var command = new SqlCommand(Sql, connection);
+			command.Parameters.AddWithValue("@regionName", regionName);
+			command.Parameters.AddWithValue("@id", id);
+			command.ExecuteNonQuery();
+		}
+	}
+}
---
+++ Z1_ADONET/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Data.SqlClient;
+namespace ADONET_1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+			var sql = "SELECT * FROM Customers";
+
+				
+			using var connection = new SqlConnection(connectionString);
+			var command = new SqlCommand(sql, connection);
+			var db = new DB();
+			connection.Open();
+			db.Select(connection);
+			Console.ReadLine();
+			db.Insert(connection, 10, "Bielsko");
+			Console.ReadLine();
+			db.Delete(connection, 10);
+			Console.ReadLine();
+			db.Update(connection, 4, "Andrychow");
+			Console.ReadLine();
+
+
+			connection.Close();
+		}
+	}
+}
